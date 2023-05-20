a3d031fb49231abe9cdc36af3b011e8cde46f4a8
Frederic DE MATOS <frederic@cometh.io>
2023-05-20 14:29:28 +0200

    Use metamask as provider

--- packages/client/src/mud/setupNetwork.ts
+++ packages/client/src/mud/setupNetwork.ts
@@ -1,9 +1,13 @@
 import { setupMUDV2Network } from "@latticexyz/std-client";
-import { createFastTxExecutor, createFaucetService, getSnapSyncRecords } from "@latticexyz/network";
+import {
+  createFastTxExecutor,
+  createFaucetService,
+  getSnapSyncRecords,
+} from "@latticexyz/network";
 import { getNetworkConfig } from "./getNetworkConfig";
 import { defineContractComponents } from "./contractComponents";
 import { world } from "./world";
-import { Contract, Signer, utils } from "ethers";
+import { Contract, Signer, utils, providers } from "ethers";
 import { JsonRpcProvider } from "@ethersproject/providers";
 import { IWorld__factory } from "contracts/types/ethers-contracts/factories/IWorld__factory";
 import { getTableIds } from "@latticexyz/utils";
@@ -14,7 +18,10 @@ export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
 export async function setupNetwork() {
   const contractComponents = defineContractComponents(world);
   const networkConfig = await getNetworkConfig();
-  const result = await setupMUDV2Network<typeof contractComponents, typeof storeConfig>({
+  const result = await setupMUDV2Network<
+    typeof contractComponents,
+    typeof storeConfig
+  >({
     networkConfig,
     world,
     contractComponents,
@@ -23,8 +30,11 @@ export async function setupNetwork() {
     worldAbi: IWorld__factory.abi,
   });
 
+  const provider = new providers.Web3Provider((window as any).ethereum);
+  await provider.send("eth_requestAccounts", []);
+
   // Request drip from faucet
-  const signer = result.network.signer.get();
+  const signer = provider.getSigner();
   if (networkConfig.faucetServiceUrl && signer) {
     const address = await signer.getAddress();
     console.info("[Dev Faucet]: Player address -> ", address);
@@ -48,10 +58,12 @@ export async function setupNetwork() {
     setInterval(requestDrip, 20000);
   }
 
-  const provider = result.network.providers.get().json;
   const signerOrProvider = signer ?? provider;
   // Create a World contract instance
-  const worldContract = IWorld__factory.connect(networkConfig.worldAddress, signerOrProvider);
+  const worldContract = IWorld__factory.connect(
+    networkConfig.worldAddress,
+    signerOrProvider
+  );
 
   if (networkConfig.snapSync) {
     const currentBlockNumber = await provider.getBlockNumber();
@@ -71,7 +83,9 @@ export async function setupNetwork() {
   // Create a fast tx executor
   const fastTxExecutor =
     signer?.provider instanceof JsonRpcProvider
-      ? await createFastTxExecutor(signer as Signer & { provider: JsonRpcProvider })
+      ? await createFastTxExecutor(
+          signer as Signer & { provider: JsonRpcProvider }
+        )
       : null;
 
   // TODO: infer this from fastTxExecute signature?
@@ -83,7 +97,9 @@ export async function setupNetwork() {
     }
   ) => Promise<ReturnType<C[F]>>;
 
-  function bindFastTxExecute<C extends Contract>(contract: C): BoundFastTxExecuteFn<C> {
+  function bindFastTxExecute<C extends Contract>(
+    contract: C
+  ): BoundFastTxExecuteFn<C> {
     return async function (...args) {
       if (!fastTxExecutor) {
         throw new Error("no signer");
